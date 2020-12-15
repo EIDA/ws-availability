63c88d2e1296fb825283ec2bdbabf6be14bf1127
Jarek Bienkowski <jaroslaw.bienkowski@hotmail.com>
2020-12-15 09:05:34 +0100

    WIP changes in the fdsn client

--- apps/fdsn_client.py
+++ apps/fdsn_client.py
@@ -3,10 +3,13 @@ import xml.etree.ElementTree as ET
 import gzip
 import logging
 
-from xml.etree.ElementTree import ParseError
+import xml.etree.ElementTree as ET
+
+from datetime import datetime
 
 FDSN_STATION_URL = "http://eida-federator.ethz.ch/fdsnws/station/1/query"
 NO_FDSNWS_DATA = "n/a"
+ENCODING = "utf-8"
 NSMAP = {"mw": "http://www.fdsn.org/xml/station/1"}
 
 
@@ -14,6 +17,16 @@ class FdsnClient:
     def _post(self, url, body):
         try:
             req = requests.post(url, data=body, allow_redirects=True)
+            req.encoding = ENCODING
+            return req.content
+        except Exception as e:
+            logging.exception(e)
+            return None
+
+    def _get(self, url):
+        try:
+            req = requests.get(url, allow_redirects=True)
+            req.encoding = ENCODING
             return req.content
         except Exception as e:
             logging.exception(e)
@@ -31,10 +44,14 @@ class FdsnClient:
             []: Same list of streams, but with updated restricted status.
         """
 
-        body = "level=station\n"
-
+        # body = "level=station\n"
+        
+        net_stat = set()
         for s in streams:
-            body += f"{s[0]} {s[1]} * * {s[6].isoformat()} {s[7].isoformat()} \n"
-
-        response = self._post(FDSN_STATION_URL, body)
+            net_stat.add(f"?{s[0]} {s[1]} * * {s[6]} {s[7]}")
+        
+        for ns in net_stat:
+            url = FDSN_STATION_URL + ns
+            resp = self._get(url)
+            event_root = ET.fromstring(resp)
         return response
